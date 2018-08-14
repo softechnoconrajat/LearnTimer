ed9542d22f75fe25ed2e7974ced522dbb3b6a1b9
Rajat Kumar Sinha <sinharajatit@hotmail.com>
2018-08-14 16:16:22 +0530

    Color Module Integrated with the code

--- LearnTimer/ViewController.swift
+++ LearnTimer/ViewController.swift
@@ -42,9 +42,9 @@ var obj2 = abc()
     
         
         
-    obj1.startTimer(timer1, 15)
+    obj1.startTimer(timer1, 15,1)
         
-    obj2.startTimer(timer2, 70)
+    obj2.startTimer(timer2, 70, 2)
         
 //
     }
@@ -99,11 +99,17 @@ class abc {
     
     var time = 0
     
-    func startTimer(_ label : UILabel, _ vechTime: Int){
+    var status = 0
+    
+    func startTimer(_ label : UILabel, _ vechTime: Int, _ Vechstatus: Int){
         
         labelName = label
         
         time = vechTime
+        
+        status = Vechstatus
+        
+        
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(abc.action), userInfo: nil, repeats: true)
     }
@@ -130,9 +136,52 @@ class abc {
             label.text =  "\(b)  :  \(a)"
         }
         
-        
+        colorChange(a, b, label, status)
         
     }
+    
+    func colorChange(_ a:Int, _ b:Int, _ label:UILabel, _ status:Int) {
+        
+        let a : Int = a
+        let b : Int = b
+        let label :UILabel = label
+        let status: Int = status
+        
+        DispatchQueue.main.async {
+            
+            if status == 1{
+                if ( b == 0 && a < 45) {
+                    label.backgroundColor = UIColor.green
+                }
+                else if (b == 0 && a > 45) || (b == 0 && a < 59) {
+                    label.backgroundColor = UIColor.yellow
+                }
+                else if b>0 {
+                    label.backgroundColor = UIColor.red
+                }
+                
+                
+            }//end of status == 1
+                
+            else if status == 2{
+                
+                if (b <= 1 && a < 60) || (b == 2 && a < 30) {
+                    label.backgroundColor = UIColor.green
+                }
+                else if (b == 2 && a > 30) || (b == 2 && a < 59) {
+                    label.backgroundColor = UIColor.yellow
+                }
+                else {
+                    label.backgroundColor = UIColor.red
+                }
+            } //end os status == 2
+        }
+        
+        
+    } // End of colour change function
+    
+    
+    
 }
     
 
